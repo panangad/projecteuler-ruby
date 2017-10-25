2179a0e7fd834c0b5748c358f971dd68ed6ccd6c
Manuel Johnson <manuel.johnson@visualiq.com>
2017-10-25 17:00:35 +0530

    Problems till 20 added

--- problems/problem_015.rb
+++ problems/problem_015.rb
@@ -10,20 +10,27 @@ How many such routes are there through a 20×20 grid?
 
 =end
 
-def rt_ct(n)
-  (0..n).map{|x| (0..n).map{|y| [x,y] } }.count
+#Till 18*18 can be found with get_path_ct(18, 18)
+def get_path_ct(a,b)
+  if(a == 0 && b == 0)
+    return 1;
+  end
+  if(a == 0)
+    return get_path_ct(a,b-1)
+  end
+  if(b == 0)
+    return get_path_ct(a-1, b)
+  end
+  if(a == b)
+   return 2 * get_path_ct(a, b-1)
+  end  
+  return get_path_ct(a, b-1) + get_path_ct(a-1, b)  
 end
 
-00 01 11
-00 10 11
+#formula is C(2*n, n) from https://en.wikipedia.org/wiki/Lattice_path
 
-00 01 02 12 22
-00 01 11 12 22
-00 01 11 21 22
-00 10 11 12 22
-00 10 11 21 22
-00 10 20 21 22
+def ncr(n, r)
+ (1..n).reduce(&:*) / ((1..r).reduce(&:*) * (1..(n-r)).reduce(&:*))
+end
 
-4/2
-12/2
-
+puts ncr(40,20)
