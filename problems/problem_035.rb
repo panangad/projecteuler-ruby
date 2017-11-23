334f45b40ffe15c7c5f9c768ab0596a8388d1683
Manuel Johnson <manuel.johnson@visualiq.com>
2017-11-23 13:12:20 +0530

    prob 35

--- problems/problem_035.rb
+++ problems/problem_035.rb
@@ -10,3 +10,20 @@ How many circular primes are there below one million?
 
 =end
 
+require 'prime'
+
+def allrots(n)
+  ln = n.to_s.length
+  ml = 10**(ln-1)
+  ln.times.map{|x| t = n % 10; n = n / 10; n = n + t * ml}
+end
+
+def ifallprime(n)
+  return false if n.to_s =~ /[02468]/
+  return false if !n.prime?
+  nnn = allrots(n)
+  rs = nnn[0..-2].detect{|x| !x.prime? }
+  !rs
+end
+
+puts (3..1000000).step(2).select{|x| ifallprime(x) }.count + 1
